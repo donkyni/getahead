75377c87b96761b986596f19fddc65a906bf9789
donkyni <49393917+donkyni@users.noreply.github.com>
2021-03-23 11:29:34 +0000

    local 1002

--- getahead/settings.py
+++ getahead/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'getahead',
         'USER': 'getahead',
-        'PASSWORD': 'getahead',
+        'PASSWORD': 't1o5o0r7!@#',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
