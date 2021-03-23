445f248e5c2d88d05187d93708fd7f655c404318
Donald Sognikin <donaldsognikin54@gmail.com>
2021-03-23 09:13:01 +0100

    Root commit 23/02

--- getahead/settings.py
+++ getahead/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'getahead',
-        'USER': 'postgres',
-        'PASSWORD': 't1o5o0r7!@#',
+        'USER': 'getahead',
+        'PASSWORD': 'getahead',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
